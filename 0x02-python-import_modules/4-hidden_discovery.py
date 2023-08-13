79263827a0e4cdb57f28ec3b3b9623b41bb3c2eb
Dexter <dexter.dev17@gmail.com>
2023-08-13 15:34:32 +0000

    Hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4 as args
     names = dir(args)
     for i in names:
-        if i[:2] != '__':
-            print("{}".format(i))
+        if i[:2] != "__":
+            print(i)
