862d8b28a4dba656701787d7482688db95b52b23
Dexter <dexter.dev17@gmail.com>
2023-08-15 00:38:16 +0000

    tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,12 +2,37 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     t_a = tuple_a
     t_b = tuple_b
-    if len(t_a) < 2:
-        if t_a[0] == None:
-            t_a[0] = 0
-        elif t_a[1] == None:
-            t_a[1] == 0
-    elif len
-    a = t_a[0] + t_b[0]
-    b = t_a[1] + t_b[1]
-    return (a, b)
+    # if both tuples are of lenght 0
+    if len(t_a) == 0 and len(t_b) == 0:
+        return (0, 0)
+
+    # if both tuples are of lenght 1
+    elif len(t_a) == 1 and len(t_b) == 1:
+        return (t_a[0] + t_b[0], 0)
+
+    # if tuple a is 1 and tuple b is 2
+    elif len(t_a) == 1 and len(t_b) >= 2:
+        sum_a = t_a[0] + t_b[0]
+        sum_b = t_b[1]
+
+    # if tuple a is 2 and tuple b is 1
+    elif len(t_b) == 1 and len(t_a) >= 2:
+        sum_a = t_a[0] + t_b[0]
+        sum_b = t_a[1]
+
+    # if both tuples are of lenght 2 or more
+    elif len(t_a) >= 2 and len(t_b) >= 2:
+        sum_a = t_a[0] + t_b[0]
+        sum_b = t_a[1] + t_b[1]
+
+    # if t_a is 2 and t_b is 0
+    elif len(t_a) >= 2 and len(t_b) == 0:
+        sum_a = t_a[0]
+        sum_b = t_a[1]
+
+    # if t_a is 2 and t_b is 0
+    elif len(t_b) >= 2 and len(t_a) == 0:
+        sum_a = t_b[0]
+        sum_b = t_b[1]
+
+    return (sum_a, sum_b)
