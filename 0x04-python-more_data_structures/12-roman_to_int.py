2279ceaaec23cfaed1ccfc043bce3d2dcc47a20f
Dexter <dexter.dev17@gmail.com>
2023-08-25 21:06:01 +0000

    Roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 dich = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-def roman_to_int(roman_string:str):
+def roman_to_int(roman_string):
     result = 0
-    num = 0  
+    num = 0
+    res = isinstance(roman_string, str)
+    if not res:
+        return 0
     for i in roman_string[::-1]:
         value = dich[i]
         if value >= num:
